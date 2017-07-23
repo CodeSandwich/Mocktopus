35fadf7896589d1222270e4e04d8dcff9ea49273
CodeSandwich <igor.zuk@protonmail.com>
2017-07-23 13:55:21 +0200

    Add method support in HeaderBuilder

--- injector/src/header_builder.rs
+++ injector/src/header_builder.rs
@@ -1,4 +1,6 @@
-use syn::{ExprKind, FnArg, Generics, Ident, Pat, self, Stmt};
+use syn::{ExprKind, FnArg, Generics, Ident, Mutability, Pat, self, Stmt};
+
+const ARG_REPLACEMENT_TUPLE_NAME: &str = "replacement";
 
 macro_rules! error_msg {
     ($msg:expr) => { concat!("Mocktopus internal error: ", $msg) }
@@ -55,34 +57,29 @@ impl<'a> HeaderBuilder<'a> {
     fn create_header_block_str(&self) -> String {
         format!(
             r#"{{
-            let ({non_self_args}) = {{
+            let ({non_self_args}) = {block_unsafety} {{
                 use mocktopus::*;
                 match Mockable::call_mock(&{full_fn_name}, (({self_arg}{non_self_args}))) {{
-                    MockResult::Continue(input) => input,
+                    MockResult::Continue({arg_replacement_tuple}) => {{
+                        {self_arg_replacement}
+                        {non_self_arg_return}
+                    }},
                     MockResult::Return(result) => return result,
                 }}
             }};
         }}"#,
+            block_unsafety = self.create_block_unsafety_str(),
+            full_fn_name = self.create_full_fn_name_str(),
             self_arg = self.create_self_arg_str(),
             non_self_args = self.create_non_self_args_str(),
-            full_fn_name = self.create_full_fn_name_str())
+            arg_replacement_tuple = ARG_REPLACEMENT_TUPLE_NAME,
+            self_arg_replacement = self.create_self_replacement_str(),
+            non_self_arg_return = self.create_non_self_return_str())
     }
 
-    fn create_non_self_args_str(&self) -> String {
-        let mut input_args_str = String::new();
-        for arg in self.non_self_args.expect(error_msg!("inputs not set")) {
-            match *arg {
-                FnArg::Captured(Pat::Ident(_, ref ident, None), _) => input_args_str.push_str(ident.as_ref()),
-                _ => panic!(error_msg!("invalid function input '{:?}'"), arg),
-            };
-            input_args_str.push_str(", ");
-        };
-        input_args_str
-    }
-
-    fn create_self_arg_str(&self) -> &str {
+    fn create_block_unsafety_str(&self) -> &str {
         match self.self_arg {
-            Some(_) => "self, ",
+            Some(_) => "unsafe",
             None => "",
         }
     }
@@ -92,6 +89,59 @@ impl<'a> HeaderBuilder<'a> {
                 if self.is_method { "Self::" } else { "" },
                 self.fn_ident.expect(error_msg!("fn name not set")).as_ref(),
                 create_generics_str(self.fn_generics))
+    }
+
+    fn create_self_arg_str(&self) -> String {
+        match self.self_arg {
+            Some(_) => format!("mem::replace(&mut {}, mem::uninitialized())", self.create_mut_self_acqusition_str()),
+            None => "".to_string(),
+        }
+    }
+
+    fn create_non_self_args_str(&self) -> String {
+        let mut input_args_str = String::new();
+        for arg in self.non_self_args.expect(error_msg!("passed inputs not set")) {
+            match *arg {
+                FnArg::Captured(Pat::Ident(_, ref ident, None), _) => input_args_str.push_str(ident.as_ref()),
+                _ => panic!(error_msg!("invalid function input '{:?}'"), arg),
+            };
+            input_args_str.push_str(", ");
+        };
+        input_args_str
+    }
+
+    fn create_self_replacement_str(&self) -> String {
+        match self.self_arg {
+            Some(_) => format!("{} = {}.0;", self.create_mut_self_acqusition_str(), ARG_REPLACEMENT_TUPLE_NAME),
+            None => "".to_string(),
+        }
+    }
+
+    fn create_non_self_return_str(&self) -> String {
+        match self.self_arg {
+            Some(_) => {
+                let mut non_self_return_str = "(".to_string();
+                for i in 0..self.non_self_args.expect(error_msg!("returned inputs not set")).len() {
+                    non_self_return_str.push_str(&format!("{}.{}, ", ARG_REPLACEMENT_TUPLE_NAME, i + 1));
+                }
+                non_self_return_str.push(')');
+                non_self_return_str
+            },
+            None => ARG_REPLACEMENT_TUPLE_NAME.to_string()
+        }
+    }
+
+    fn create_mut_self_acqusition_str(&self) -> String {
+        format!("*(&self as *const {} as *mut {0}", self.create_self_type_str())
+    }
+
+    fn create_self_type_str(&self) -> &str {
+        match self.self_arg {
+            Some(&FnArg::SelfRef(_, Mutability::Immutable)) => "&Self",
+            Some(&FnArg::SelfRef(_, Mutability::Mutable)) => "&mut Self",
+            Some(&FnArg::SelfValue(_)) => "Self",
+            _ => panic!(error_msg!("invalid self arg: '{:?}'"), self.self_arg),
+        }
     }
 }
 
--- tests/todo.rs
+++ tests/todo.rs
@@ -1,9 +1,13 @@
 #[test]
-fn methods() {
+fn method_experiments() {
+    let s4 = S(4);
+    let mut s5 = S(5);
     assert_eq!(200, S(2).f());
     assert_eq!(300, S(3).f_mut());
-    assert_eq!(400, S(4).f_ref(&S(400)));
-    assert_eq!(500, S(5).f_ref_mut(&mut S(500)));
+    assert_eq!(400, s4.f_ref(&S(400)));
+    assert_eq!(4,   s4.0);
+    assert_eq!(500, s5.f_ref_mut(&mut S(500)));
+    assert_eq!(5,   s5.0);
 }
 
 struct S(u32);
@@ -13,66 +17,50 @@ use std::mem;
 impl <'a> S {
     fn f(self) -> u32 {
         unsafe {
-            *(&self as *const Self as *mut Self) =
-                mem::replace(&mut*(&self as *const Self as *mut Self), mem::uninitialized())
-                    .s();
+            let moved_self = mem::replace(&mut*(&self as *const Self as *mut Self), mem::uninitialized());
+            let new_self = moved_self.s();
+            *(&self as *const Self as *mut Self) = new_self;
+//            *(&self as *const Self as *mut Self) =
+//                mem::replace(&mut*(&self as *const Self as *mut Self), mem::uninitialized())
+//                    .s();
         }
         self.0
     }
 
     fn f_mut(mut self) -> u32 {
-//        self = self.s();
         unsafe {
-            *(&self as *const Self as *mut Self) =
-                mem::replace(&mut*(&self as *const Self as *mut Self), mem::uninitialized())
-                    .s();
+            let moved_self = mem::replace(&mut*(&self as *const Self as *mut Self), mem::uninitialized());
+            let new_self = moved_self.s();
+            *(&self as *const Self as *mut Self) = new_self;
+//            *(&self as *const Self as *mut Self) =
+//                mem::replace(&mut*(&self as *const Self as *mut Self), mem::uninitialized())
+//                    .s();
         }
         self.0
     }
 
     fn f_ref(&'a self, other: &'a Self) -> u32 {
         unsafe {
-            *(&self as *const &Self as *mut &Self) =
-                mem::replace(&mut*(&self as *const &Self as *mut &Self), mem::uninitialized())
-                    .t(other);
+            let moved_self = mem::replace(&mut*(&self as *const &Self as *mut &Self), mem::uninitialized());
+            let new_self = moved_self.t(other);
+            *(&self as *const &Self as *mut &Self) = new_self;
+//            *(&self as *const &Self as *mut &Self) =
+//                mem::replace(&mut*(&self as *const &Self as *mut &Self), mem::uninitialized())
+//                    .t(other);
         }
         self.0
     }
 
-//    let header_str = format!(
-//        r#"{{
-//            let ({}) = {{
-//                use mocktopus::*;
-//                match Mockable::call_mock(&{}, (({}))) {{
-//                    MockResult::Continue(input) => input,
-//                    MockResult::Return(result) => return result,
-//                }}
-//            }};
-//        }}"#, original_arg_names, full_fn_name, unignored_arg_names);
-
-//    let header_str = format!(
-//        r#"{{
-//            #[allow(unused_unsafe)]
-//            let ({}) = unsafe {{  <= shadow list must be trimmed of self
-//                use mocktopus::*;
-//                match Mockable::call_mock(&{}, (({}))) {{ <= unignored_arg_names have self special treated
-//                    MockResult::Continue(input) => { <= generated from arg list
-//                        restore self
-//                        (input 1..), <= trimmed of self
-//                      }
-//                    MockResult::Return(result) => return result,
-//                }}
-//            }};
-//        }}"#, original_arg_names, full_fn_name, unignored_arg_names);
-
     fn f_ref_mut(&'a mut self, other: &'a mut Self) -> u32 {
-        unsafe {
-//            let self_mut_ref = &self as *const &mut Self as *mut &mut Self;
-//            let self_val = mem::replace(&mut*(&self as *const &mut Self as *mut &mut Self), mem::uninitialized());
-            let results = Self::u(mem::replace(&mut*(&self as *const &mut Self as *mut &mut Self), mem::uninitialized()), other);
-            *(&self as *const &mut Self as *mut &mut Self) = results;
-            // reteurn the rest of result
-
+        unsafe { // 'unsafe' IF SELF
+            //A
+            let new_self = Self::u(mem::replace(&mut*(&self as *const &mut Self as *mut &mut Self), mem::uninitialized()), other);
+            *(&self as *const &mut Self as *mut &mut Self) = new_self; // IF SELF
+            //B
+//            let moved_self = mem::replace(&mut*(&self as *const &mut Self as *mut &mut Self), mem::uninitialized());
+//            let new_self = moved_self.u(other);
+//            *(&self as *const &mut Self as *mut &mut Self) = new_self;
+            //C
 //            *(&self as *const &mut Self as *mut &mut Self) =
 //                mem::replace(&mut*(&self as *const &mut Self as *mut &mut Self), mem::uninitialized())
 //                    .u(other);
