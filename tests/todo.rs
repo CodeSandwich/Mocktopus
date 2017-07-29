5399b1de88015fba65bae0c922fbc8c38d354618
CodeSandwich <igor.zuk@protonmail.com>
2017-07-30 01:13:05 +0200

    Add trait mocking experiments

--- tests/todo.rs
+++ tests/todo.rs
@@ -1,82 +1,101 @@
-#[test]
-fn method_experiments() {
-    let s4 = S(4);
-    let mut s5 = S(5);
-    assert_eq!(200, S(2).f());
-    assert_eq!(300, S(3).f_mut());
-    assert_eq!(400, s4.f_ref(&S(400)));
-    assert_eq!(4,   s4.0);
-    assert_eq!(500, s5.f_ref_mut(&mut S(500)));
-    assert_eq!(5,   s5.0);
+extern crate mocktopus;
+
+use mocktopus::*;
+use std::fmt::Debug;
+
+trait Trait {
+    fn f(x: u32) -> u32;
+    fn g(x: u32) -> u32 {
+        let x = match mocktopus::Mockable::call_mock(&<Self as Trait>::g, (x,)) {
+            mocktopus::MockResult::Continue((xx,)) => xx,
+            mocktopus::MockResult::Return(xx) => return xx,
+        };
+        x * 2
+    }
 }
 
-struct S(u32);
+//TODO TEST TRAIT DEFAULT LEAKAGE
+//TODO TEST REGULAR AND TRAIT METHODS LEAKAGE
 
-use std::mem;
 
-impl <'a> S {
-    fn f(self) -> u32 {
-        unsafe {
-            let moved_self = mem::replace(&mut*(&self as *const Self as *mut Self), mem::uninitialized());
-            let new_self = moved_self.s();
-            *(&self as *const Self as *mut Self) = new_self;
-//            *(&self as *const Self as *mut Self) =
-//                mem::replace(&mut*(&self as *const Self as *mut Self), mem::uninitialized())
-//                    .s();
-        }
-        self.0
+struct StructG<T>(T);
+
+struct StructG2<T>(T);
+
+impl <T> Into<StructG2<T>> for StructG<T> {
+    fn into(mut self) -> StructG2<T> {
+        self = match mocktopus::Mockable::call_mock(&Self::into, (self,)) {
+            mocktopus::MockResult::Continue((xx,)) => xx,
+            mocktopus::MockResult::Return(xx) => return xx,
+        };
+        StructG2(self.0)
     }
+}
 
-    fn f_mut(mut self) -> u32 {
-        unsafe {
-            let moved_self = mem::replace(&mut*(&self as *const Self as *mut Self), mem::uninitialized());
-            let new_self = moved_self.s();
-            *(&self as *const Self as *mut Self) = new_self;
-//            *(&self as *const Self as *mut Self) =
-//                mem::replace(&mut*(&self as *const Self as *mut Self), mem::uninitialized())
-//                    .s();
-        }
-        self.0
-    }
+struct Struct();
 
-    fn f_ref(&'a self, other: &'a Self) -> u32 {
-        unsafe {
-            let moved_self = mem::replace(&mut*(&self as *const &Self as *mut &Self), mem::uninitialized());
-            let new_self = moved_self.t(other);
-            *(&self as *const &Self as *mut &Self) = new_self;
-//            *(&self as *const &Self as *mut &Self) =
-//                mem::replace(&mut*(&self as *const &Self as *mut &Self), mem::uninitialized())
-//                    .t(other);
-        }
-        self.0
+impl Struct {
+    fn into(self) -> u32 {
+        match mocktopus::Mockable::call_mock(&Self::into, (self,)) {
+            mocktopus::MockResult::Continue((xx,)) => xx,
+            mocktopus::MockResult::Return(xx) => return xx,
+        };
+        42
     }
+}
 
-    fn f_ref_mut(&'a mut self, other: &'a mut Self) -> u32 {
-        unsafe { // 'unsafe' IF SELF
-            //A
-            let new_self = Self::u(mem::replace(&mut*(&self as *const &mut Self as *mut &mut Self), mem::uninitialized()), other);
-            *(&self as *const &mut Self as *mut &mut Self) = new_self; // IF SELF
-            //B
-//            let moved_self = mem::replace(&mut*(&self as *const &mut Self as *mut &mut Self), mem::uninitialized());
-//            let new_self = moved_self.u(other);
-//            *(&self as *const &mut Self as *mut &mut Self) = new_self;
-            //C
-//            *(&self as *const &mut Self as *mut &mut Self) =
-//                mem::replace(&mut*(&self as *const &mut Self as *mut &mut Self), mem::uninitialized())
-//                    .u(other);
-        }
-        self.0
+impl Into<u32> for Struct {
+    fn into(self) -> u32 {
+        match mocktopus::Mockable::call_mock(&<Self as Into<u32>>::into, (self,)) {
+            mocktopus::MockResult::Continue((xx,)) => xx,
+            mocktopus::MockResult::Return(xx) => return xx,
+        };
+        42
     }
+}
 
-    fn s(self) -> Self {
-        S(self.0 * 100)
+impl Into<f64> for Struct {
+    fn into(self) -> f64 {
+        match mocktopus::Mockable::call_mock(&<Self as Into<f64>>::into, (self,)) {
+            mocktopus::MockResult::Continue((xx,)) => xx,
+            mocktopus::MockResult::Return(xx) => return xx,
+        };
+        42.
     }
+}
 
-    fn t(&'a self, other: &'a Self) -> &'a Self {
-        other
+impl Trait for Struct {
+    fn f(x: u32) -> u32 {
+        let x = match mocktopus::Mockable::call_mock(&Self::f, (x,)) {
+            mocktopus::MockResult::Continue((xx,)) => xx,
+            mocktopus::MockResult::Return(xx) => return xx,
+        };
+        x * 2
     }
+}
+struct Struct2();
 
-    fn u(&'a mut self, other: &'a mut Self) -> &'a mut Self {
-        other
+impl Trait for Struct2 {
+    fn f(x: u32) -> u32 {
+        let x = match mocktopus::Mockable::call_mock(&<Self as Trait>::f, (x,)) {
+            mocktopus::MockResult::Continue((xx,)) => xx,
+            mocktopus::MockResult::Return(xx) => return xx,
+        };
+        x * 2
     }
+}
+
+#[test]
+fn trait_experiments() {
+    <Struct as Trait>::f.mock_raw(|_| MockResult::Return(123));
+    assert_eq!(123, Struct::f(3));
+    <Struct as Trait>::f.mock_raw(|x| MockResult::Continue((x+1,)));
+    assert_eq!(8, Struct::f(3));
+
+
+    <Struct as Trait>::g.mock_raw(|_| MockResult::Return(123));
+    assert_eq!(123, Struct::g(3));
+    <Struct as Trait>::g.mock_raw(|x| MockResult::Continue((x+1,)));
+    assert_eq!(8, Struct::g(3));
+    assert_eq!(6, Struct2::g(3));
 }
