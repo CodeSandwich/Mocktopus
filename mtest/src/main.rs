f6e0dc2549f86cb4939ab611721cf617d7bc2ca1
CodeSandwich <igor.zuk@protonmail.com>
2018-03-24 22:11:51 +0100

    Move copying packages into PackageCopier

--- mtest/src/main.rs
+++ mtest/src/main.rs
@@ -3,28 +3,97 @@
 extern crate cargo_edit;
 extern crate cargo_metadata;
 extern crate data_encoding;
+extern crate filetime;
 extern crate fs_extra;
 
 use cargo_edit::{Dependency, Manifest};
 use cargo_metadata::{Metadata, Package};
 use data_encoding::BASE64URL_NOPAD;
+use filetime::FileTime;
 use fs_extra::dir::CopyOptions;
+use std::collections::{HashMap, HashSet};
 use std::fmt::Write;
 use std::fs;
 use std::iter;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 const MOCKTOPUS_DIR: &str = ".mocktopus";
 
 fn main() {
     let metadata = cargo_metadata::metadata_deps(None, true).expect("0");
-    let workspace_target = create_workspace_target(&metadata);
+    let mut package_copier = PackageCopier::new(&metadata);
     for package in &metadata.packages {
         println!("     Mocking {}", package.id);
-        let package_path = copy_package(package, &workspace_target);
+        let package_path = package_copier.copy_package(package);
         inject_manifest(package_path, &package.id, &metadata);
     }
     println!("Finished mocking");
+}
+
+struct PackageCopier {
+    old_files:  HashMap<PathBuf, FileTime>,
+    old_dirs:   HashSet<PathBuf>,
+    root:       PathBuf,
+}
+
+impl PackageCopier {
+    pub fn new(metadata: &Metadata) -> Self {
+        let mut copier = PackageCopier {
+            old_files:  HashMap::new(),
+            old_dirs:   HashSet::new(),
+            root:       PathBuf::new(),
+        };
+        let mut root = PathBuf::from(&metadata.workspace_root);
+        root.push(MOCKTOPUS_DIR);
+        if !root.is_dir() {
+            fs::create_dir(&root).expect("13")
+        }
+        copier.fill_from_dir(&root);
+        copier.root = root;
+        copier
+    }
+
+    fn fill_from_dir<P: AsRef<Path>>(&mut self, dir: P) {
+        for dir_entry_res in fs::read_dir(dir).expect("14") {
+            let dir_entry = dir_entry_res.expect("15");
+            let path = dir_entry.path();
+            let metadata = dir_entry.metadata().expect("16");
+            if metadata.is_dir() {
+                self.fill_from_dir(&path);
+                self.old_dirs.insert(path);
+            } else if metadata.is_file() {
+                self.old_files.insert(path, FileTime::from_last_modification_time(&metadata));
+            }
+        }
+    }
+
+    pub fn copy_package(&mut self, package: &Package) -> PathBuf {
+        let copy_opts = CopyOptions {
+            copy_inside: true,
+            ..CopyOptions::new()
+        };
+        let sources = fs::read_dir(PathBuf::from(&package.manifest_path).parent().expect("14"))
+            .expect("15")
+            .map(|res| res.expect("16"))
+            .filter(|entry| entry.file_name() != *MOCKTOPUS_DIR && entry.file_name() != *"target")
+            .map(|entry| entry.path())
+            .collect();
+        let target = self.root.join(encode_id(&package.id));
+        fs_extra::dir::create(&target, true)
+            .expect("17");
+        fs_extra::copy_items(&sources, &target, &copy_opts)
+            .expect("18");
+        target
+    }
+
+    pub fn remove_old(self) {
+        self.old_dirs.iter()
+            .filter(|dir| dir.exists())
+            .for_each(|dir| fs::remove_dir_all(dir).expect("18"));
+        self.old_files.keys()
+            .filter(|file| file.exists())
+            .for_each(|file| fs::remove_file(file).expect("19"));
+    }
 }
 
 fn inject_manifest(package_path: PathBuf, package_id: &str, metadata: &Metadata) {
@@ -88,33 +157,6 @@ fn id_to_dep_path(id: &str) -> String {
         .into_os_string()
         .into_string()
         .unwrap()
-}
-
-fn create_workspace_target(metadata: &Metadata) -> PathBuf {
-    let mut workspace_target = PathBuf::from(&metadata.workspace_root);
-    workspace_target.push(MOCKTOPUS_DIR);
-    fs_extra::dir::create(&workspace_target, true)
-        .expect("13");
-    workspace_target
-}
-
-fn copy_package(package: &Package, workspace_target: &PathBuf) -> PathBuf {
-    let copy_opts = CopyOptions {
-        copy_inside: true,
-        ..CopyOptions::new()
-    };
-    let sources = fs::read_dir(PathBuf::from(&package.manifest_path).parent().expect("14"))
-        .expect("15")
-        .map(|res| res.expect("16"))
-        .filter(|entry| entry.file_name() != *MOCKTOPUS_DIR && entry.file_name() != *"target")
-        .map(|entry| entry.path())
-        .collect();
-    let target = workspace_target.join(encode_id(&package.id));
-    fs_extra::dir::create(&target, true)
-        .expect("17");
-    fs_extra::copy_items(&sources, &target, &copy_opts)
-        .expect("18");
-    target
 }
 
 fn encode_id(id_str: &str) -> String {
