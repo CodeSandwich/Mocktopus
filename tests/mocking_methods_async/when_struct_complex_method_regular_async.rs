ff3e3e8578ec7e9f7a4f2b948462bfda43e613b8
Gregory Hill <gregorydhill@outlook.com>
2020-12-22 18:44:32 +0000

    fix mock async macro call substitution
    Signed-off-by: Gregory Hill <gregorydhill@outlook.com>

--- tests/mocking_methods_async/when_struct_complex_method_regular_async.rs
+++ tests/mocking_methods_async/when_struct_complex_method_regular_async.rs
@@ -2,6 +2,10 @@ use super::*;
 
 struct Struct(u8);
 
+fn plus_one(value: u8) -> u8 {
+    value + 1
+}
+
 #[mockable]
 impl Struct {
     async fn ref_method_with_binding(&self, arg: bool) -> String {
@@ -12,6 +16,10 @@ impl Struct {
 
     async fn ref_method_with_ref(&self, arg: &bool) -> String {
         format!("{} {}", self.0, arg)
+    }
+
+    async fn ref_method_with_call(&self, arg: bool) -> String {
+        format!("{} {}", plus_one(self.0), arg)
     }
 }
 
@@ -71,6 +79,41 @@ mod and_method_is_ref_method_with_ref {
         }
 
         assert_eq!("mocked 2 true", struct_2.ref_method_with_ref(&true).await);
+        assert_eq!(2, struct_2.0);
+    }
+}
+
+mod and_method_is_ref_method_with_call {
+    use super::*;
+
+    #[tokio::test]
+    async fn and_not_mocked_then_runs_normally() {
+        assert_eq!("3 true", Struct(2).ref_method_with_call(true).await);
+    }
+
+    #[tokio::test]
+    async fn and_continue_mocked_then_runs_with_modified_args() {
+        let struct_2 = Struct(2);
+        let struct_3 = Struct(3);
+        unsafe {
+            Struct::ref_method_with_call.mock_raw(|_, b| MockResult::Continue((&struct_3, !b)));
+        }
+
+        assert_eq!("4 false", struct_2.ref_method_with_call(true).await);
+        assert_eq!(2, struct_2.0);
+        assert_eq!(3, struct_3.0);
+    }
+
+    #[tokio::test]
+    async fn and_return_mocked_then_returns_mocking_result() {
+        let struct_2 = Struct(2);
+        unsafe {
+            Struct::ref_method_with_call.mock_raw(|a, b| {
+                MockResult::Return(Box::pin(async move { format!("mocked {} {}", a.0, b) }))
+            });
+        }
+
+        assert_eq!("mocked 2 true", struct_2.ref_method_with_call(true).await);
         assert_eq!(2, struct_2.0);
     }
 }
