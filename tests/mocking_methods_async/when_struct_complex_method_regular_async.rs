aa93669b731074e47bf47628af1f299752f69901
Gregory Hill <gregorydhill@outlook.com>
2021-04-02 12:48:26 +0100

    mock associated async functions, ignore async lifetimes
    Signed-off-by: Gregory Hill <gregorydhill@outlook.com>

--- tests/mocking_methods_async/when_struct_complex_method_regular_async.rs
+++ tests/mocking_methods_async/when_struct_complex_method_regular_async.rs
@@ -21,6 +21,14 @@ impl Struct {
     async fn ref_method_with_call(&self, arg: bool) -> String {
         format!("{} {}", plus_one(self.0), arg)
     }
+
+    async fn assoc_method_with_ref(arg: &str) -> String {
+        format!("Hello {}", arg)
+    }
+
+    async fn assoc_method_with_ref_and_lifetime<'a>(arg1: &'a str, arg2: &'a str) -> String {
+        format!("{} {}", arg1, arg2)
+    }
 }
 
 mod and_method_is_ref_method_with_binding {
@@ -115,5 +123,63 @@ mod and_method_is_ref_method_with_call {
 
         assert_eq!("mocked 2 true", struct_2.ref_method_with_call(true).await);
         assert_eq!(2, struct_2.0);
+    }
+}
+
+mod and_method_is_assoc_method_with_ref {
+    use super::*;
+
+    #[tokio::test]
+    async fn and_not_mocked_then_runs_normally() {
+        assert_eq!("Hello World", Struct::assoc_method_with_ref("World").await);
+    }
+
+    #[tokio::test]
+    async fn and_continue_mocked_then_runs_with_modified_args() {
+        unsafe {
+            Struct::assoc_method_with_ref.mock_raw(|_| MockResult::Continue(("Universe",)));
+        }
+
+        assert_eq!("Hello Universe", Struct::assoc_method_with_ref("World").await);
+    }
+
+    #[tokio::test]
+    async fn and_return_mocked_then_returns_mocking_result() {
+        unsafe {
+            Struct::assoc_method_with_ref.mock_raw(|s| {
+                MockResult::Return(Box::pin(async move { format!("Welcome {}", s) }))
+            });
+        }
+
+        assert_eq!("Welcome World", Struct::assoc_method_with_ref("World").await);
+    }
+}
+
+mod and_method_is_assoc_method_with_ref_and_lifetime {
+    use super::*;
+
+    #[tokio::test]
+    async fn and_not_mocked_then_runs_normally() {
+        assert_eq!("Hello World", Struct::assoc_method_with_ref_and_lifetime("Hello", "World").await);
+    }
+
+    #[tokio::test]
+    async fn and_continue_mocked_then_runs_with_modified_args() {
+        unsafe {
+            Struct::assoc_method_with_ref_and_lifetime.mock_raw(|s1, _| MockResult::Continue((s1, "Universe",)));
+        }
+
+        assert_eq!("Hello Universe", Struct::assoc_method_with_ref_and_lifetime("Hello", "World").await);
+    }
+
+    #[tokio::test]
+    async fn and_return_mocked_then_returns_mocking_result() {
+        unsafe {
+            Struct::assoc_method_with_ref_and_lifetime.mock_raw(|_, s2| {
+                MockResult::Return(Box::pin(async move { format!("Welcome {}", s2) }))
+            });
+        }
+
+        assert_eq!("Welcome World", Struct::assoc_method_with_ref_and_lifetime("Hello", "World").await);
     }
 }
